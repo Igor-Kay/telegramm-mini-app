adc53267ee4985e69cec81cd9682e86773ca0a44
matvos <katsu600666@gmail.com>
2025-03-12 01:07:52 +0700

    avatar add

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -5,15 +5,16 @@ const Profile = () => {
   return (
     <div className="max-w-2xl mx-auto p-4 bg-gray-50 profile-box">
       <h1>Личный кабинет</h1>
-        <h3 className="user_name">"Username" <img className="user_avatar" src="./" alt="avatar"/> </h3>
-
+        <div className="user_info">
+          <h3 className="user_name">"Username" <img className="user_avatar" src="./" alt="avatar"/> </h3>
+        </div>
       <div className="inputBoxItem">
-        <h3>Логин в PlayStation</h3>
+        <h3>Логин в PayStation</h3>
         <input type="text" placeholder="Введите e-mail от турецкого аккаунта" />
       </div>
 
       <div className="inputBoxItem">
-        <h3>Пароль в PlayStation</h3>
+        <h3>Пароль в PLayStation</h3>
         <input type="text" placeholder="Введите пароль от турецкого аккаунта" />
       </div>
 
