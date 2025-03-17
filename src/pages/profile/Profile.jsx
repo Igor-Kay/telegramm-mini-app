d238ea8614e36d19f19939f78be295490f4f5a26
matvos <katsu600666@gmail.com>
2025-03-17 18:39:40 +0700

    avatar add

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -4,7 +4,12 @@ import "./Profile.scss";
 const Profile = () => {
   return (
     <div className="max-w-2xl mx-auto p-4 bg-gray-50 profile-box">
-      <img className="user_avatar" src="./" alt="avatar"/><h1>Личный кабинет "Username"</h1>
+      <Link to="./">
+        <div className="profileBox">
+          <p>Back</p>
+        </div>
+      </Link>
+      <img className="user_avatar" src="./" alt="avatar"/><h1>Личный кабинет {username}</h1>
       <div className="inputBoxItem">
         <h3>Логин в PayStation</h3>
         <input type="text" placeholder="Введите e-mail от турецкого аккаунта" />
