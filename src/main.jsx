f5914f05445eb9ced58c31a3615bcc66fcb5c1bd
matvos <katsu600666@gmail.com>
2025-03-27 06:21:37 +0700

    form

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-    <script src="https://telegram.org/js/telegram-web-app.js?56"></script>
-    
-    <script>
-      let tg = window.Telegram.WebApp;
-
-      tg.expand();
-    </script>
-
     <App />
   </React.StrictMode>
 )
