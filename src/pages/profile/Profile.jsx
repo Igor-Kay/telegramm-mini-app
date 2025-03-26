d76678bf0202e9271afd687ba93215a8e0288182
matvos <katsu600666@gmail.com>
2025-03-27 05:41:40 +0700

    form

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -5,28 +5,31 @@ const Profile = () => {
   return (
     <div className="max-w-2xl mx-auto p-4 bg-gray-50 profile-box">
       <img className="user_avatar" src="./" alt="avatar"/><h1>Личный кабинет</h1>
-      <div className="inputBoxItem">
-        <h3>Логин в PayStation</h3>
-        <input type="text" placeholder="Введите e-mail от турецкого аккаунта" />
-      </div>
+        <form action="" method="post">
+          <div className="inputBoxItem">
+            <h3>Логин в PS</h3>
+              <input type="text" id="user_email" placeholder="Введите e-mail от турецкого аккаунта" />
+          </div>
 
-      <div className="inputBoxItem">
-        <h3>Пароль в PLayStation</h3>
-        <input type="text" placeholder="Введите пароль от турецкого аккаунта" />
-      </div>
+          <div className="inputBoxItem">
+            <h3>Пароль в PS</h3>
+              <input type="text" id="user_password" placeholder="Введите пароль от турецкого аккаунта" />
+          </div>
 
-      <p>
-        Бот запомнит данные и автоматически использует при оформлении заказа
-      </p>
+          <p>
+            Бот запомнит данные и автоматически использует при оформлении заказа
+          </p>
 
-      <h2>Моя приставка</h2>
+          <h2>Моя приставка</h2>
 
-
-      <div className="selectPlatformBox">
-        <div className="selectPlatformBoxItem">PLayStation 4</div>
-        <div className="selectPlatformBoxItem">PLayStation 5</div>
-      </div>
-
+          <select name="_select_platform" id="_select_platform">
+            <option value="">Выбрать платформу</option>
+              <div className="selectPlatformBox">
+                <option value="_ps5"><div className="selectPlatformBoxItem">PS4</div></option>
+                <option value="_ps4"><div className="selectPlatformBoxItem">PS5</div></option>
+              </div>
+          </select>
+        </form> 
       <p>Бот будет составлять каталог и подбирать издания игр под вашу PlayStation</p>
 
       <h2>Мои подписки</h2>
