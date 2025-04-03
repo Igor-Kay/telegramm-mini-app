0a9ae7c85a14cac36363ec2162716ec9c21c85da
matvos <katsu600666@gmail.com>
2025-04-03 21:32:09 +0700

    commit

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import "./Profile.scss";
 
 const Profile = () => {
-  [userAvatar, setUserAvatar] = useState(null)
+/*  [userAvatar, setUserAvatar] = useState(null)
   useEffect(() => {
     const addUser = async () => {
       const userData = {
@@ -47,7 +47,7 @@ const Profile = () => {
   
       fetchData();
   
-    }, []);
+    }, []); */
   return (
     <div className="max-w-2xl mx-auto p-4 bg-gray-50 profile-box">
       <a class="_back" href="./">Назад</a>
